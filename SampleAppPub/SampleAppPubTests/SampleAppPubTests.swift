e15ae68b2408a53533cdeef7429a7d724a937d83
Derick <derick.derick@rakuten.com>
2024-11-05 15:39:31 +0800

    Create project

---
+++ SampleAppPub/SampleAppPubTests/SampleAppPubTests.swift
@@ -0,0 +1,17 @@
+//
+//  SampleAppPubTests.swift
+//  SampleAppPubTests
+//
+//  Created by Derick, Derick | RASIA on 5/11/24.
+//
+
+import Testing
+@testable import SampleAppPub
+
+struct SampleAppPubTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
